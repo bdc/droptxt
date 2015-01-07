6a230112167566ed0ff413a4137365fbe0b83ecf
bildersee <bildersee@bildersee-macbookair.roam.corp.google.com>
2015-01-07 13:05:18 -0500

    Various updates from a month ago.

--- static/app.js
+++ static/app.js
@@ -1,16 +1,56 @@
-var app = angular.module('text-sync', ['ngMaterial']);
+var app = angular.module(
+    'text-sync', ['cfp.hotkeys', 'ngCookies', 'ngMaterial']);
 
 
-app.controller('appCtrl', ['$scope', function(scope) {
+var AUTOSAVE_TIMEOUT_ = 4000;  // milliseconds
 
-  scope.tabs = [
-  // TODO remove this
-  /*
-    {name: 'calendar', path: '/notes/01%20calendar.txt'},
-    {name: 'todo', path: '/notes/02%20todo.txt'},
-    {name: 'scratch', path: '/notes/03%20scratch.txt'}
-    */
-  ];
+
+app.controller('appCtrl', [
+    '$scope', '$timeout', 'hotkeys', function(scope, $timeout, hotkeys) {
+  window.onbeforeunload = function() {
+    if (Storage) {
+      // strip out object keys starting with '$'
+      var tabsData = angular.copy(scope.tabs);
+      for (var i = 0; i < tabsData.length; i++) {
+        tabData = tabsData[i];
+        for (var key in tabData) {
+          if (key[0] == '$') {
+            delete tabData[key];
+          }
+        }
+      }
+      window.localStorage.setItem('tabs', JSON.stringify(tabsData));
+    }
+  };
+
+  if (Storage) {
+    var tabsValues = window.localStorage.getItem('tabs') || '[]';
+    scope.tabs = JSON.parse(tabsValues);
+  }
+  else {
+    scope.tabs = [];
+  }
+
+  hotkeys.bindTo(scope)
+    .add({
+      combo: 'ctrl+left',
+      allowIn: ['TEXTAREA'],
+      description: 'Change tabs to the left.',
+      callback: function() {
+        scope.selectedTabIndex =
+            (scope.selectedTabIndex - 1 + scope.tabs.length)
+            % scope.tabs.length;
+      }
+    })
+    .add({
+      combo: 'ctrl+right',
+      allowIn: ['TEXTAREA'],
+      description: 'Change tabs to the right.',
+      callback: function() {
+        scope.selectedTabIndex =
+            (scope.selectedTabIndex + 1) % scope.tabs.length;
+      }
+    });
 
   scope.pickFiles = function() {
     Dropbox.choose({
@@ -30,6 +70,24 @@ app.controller('appCtrl', ['$scope', function(scope) {
     });
   };
 
+  scope.$watch('tabs.length', function(newLength, oldLength) {
+    console.log('newLength=' + newLength + ', oldLength=' + oldLength);
+    if (newLength == oldLength) {
+      return;
+    }
+    var selection = scope.selectedTabIndex;
+    if (oldLength > newLength) {
+      if (selection > 0) {
+        selection = selection - 1;
+      }
+    }
+    if (newLength > oldLength) {
+      selection = oldLength;
+    }
+    $timeout(function() {
+      scope.selectedTabIndex = selection;
+    });
+  });
 }]);
 
 
@@ -37,7 +95,10 @@ app.directive('toolbarButton', [function() {
   return {
     replace: true,
     restrict: 'A',
-    scope: {},
+    scope: {
+      'active': '=?',
+      'click': '&'
+    },
     templateUrl: 'toolbar-button.html',
     transclude: true
   };
@@ -45,7 +106,9 @@ app.directive('toolbarButton', [function() {
 
 
 app.directive('tabbedFile', [
-    '$http', '$materialToast', function($http, toast) {
+    '$http', '$mdToast', '$timeout', 'hotkeys',
+    function($http, $mdToast, $timeout, hotkeys) {
+
   var load = function(path, scope) {
     scope.error = false;
     scope.loaded = false;
@@ -53,6 +116,7 @@ app.directive('tabbedFile', [
     .success(function(data, status, headers, config) {
       scope.loaded = true;
       scope.content = data.content;
+      scope.savedContent = data.content;
       scope.metadata = data.metadata;
     })
     .error(function(data, status, headers, config) {
@@ -61,27 +125,30 @@ app.directive('tabbedFile', [
     });
   };
 
-  var push = function(path, scope) {
+  var save = function(path, scope) {
+    var saveContent = scope.content;
     $http({
       method: 'POST',
       url: '/j/put_file' + path,
       params: {parent_rev: scope.metadata.rev},
-      data: scope.content
+      data: saveContent
     })
     .success(function(data, status, headers, config) {
       scope.metadata = data.metadata;
+      scope.savedContent = saveContent;
       var message = 'Saved!';
       if (data.conflict) {
+        // TODO something smarter here
         message += ' (with conflict)'
       }
-      toast({
-        template: '<material-toast>' + message + '</material-toast>',
+      $mdToast.show({
+        template: '<md-toast>' + message + '</md-toast>',
         position: 'bottom left'
       });
     })
     .error(function(data, status, headers, config) {
-      toast({
-        template: '<material-toast>' + 'Error!' + '</material-toast>',
+      $mdToast.show({
+        template: '<md-toast>' + 'Error!' + '</md-toast>',
         position: 'bottom left'
       });
     });
@@ -91,6 +158,7 @@ app.directive('tabbedFile', [
     replace: true,
     restrict: 'A',
     scope: {
+      'close': '&',
       'tab': '='
     },
     templateUrl: 'tabbed-file.html',
@@ -99,10 +167,24 @@ app.directive('tabbedFile', [
       scope.reload = function() {
         load(scope.tab.path, scope);
       };
-      scope.commit = function() {
-        push(scope.tab.path, scope);
+      scope.save = function() {
+        save(scope.tab.path, scope);
       };
       scope.reload();
+      scope.autosave = null;
+      scope.$watch('content', function(newContent, oldContent) {
+        if (newContent === oldContent) {
+          return;
+        }
+        $timeout.cancel(scope.autosave);
+        scope.autosave = $timeout(function() {
+          if (scope.content != scope.savedContent) {
+            scope.save();
+          }
+        }, AUTOSAVE_TIMEOUT_);
+      });
+
+      element.find('textarea').focus();  // TODO
     }
   };
 }]);
